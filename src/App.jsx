284dc28635c97162fd7e6bb8acd4739f64c97634
HADYZELLEYA\hadi_ <hadi_scorpion95@hotmail.com>
2025-01-31 00:46:27 +0200

    implemented the movies and search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "../public/logo.png";
 import banner from "../public/hero.png";
-import search from "../public/search.svg";
 import "./App.css";
 import Search from "./components/Search";
+import axios from "axios";
+import MovieCard from "./components/MovieCard";
+import Spinner from "./components/Spinner";
+import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite";
+
+const MAIN_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [decouncedSearchTerm, setDecouncedSearchTerm] = useState("");
+
+  useDebounce(
+    () => {
+      setDecouncedSearchTerm(searchTerm);
+    },
+    500,
+    [searchTerm]
+  );
+
+  const fetchMovies = async (query = "") => {
+    try {
+      setLoading(true);
+      const endpoint = query
+        ? `${MAIN_URL}/search/movie?query=${encodeURIComponent(
+            query
+          )}&api_key=${API_KEY}`
+        : `${MAIN_URL}/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`;
+      const res = await axios.get(endpoint);
+      setMovies(res?.data);
+      setLoading(false);
+
+      if (query && res?.data?.results?.length > 0) {
+        await updateSearchCount(query, res?.data?.results[0]);
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(decouncedSearchTerm);
+  }, [decouncedSearchTerm]);
+  console.log(movies);
+  if (error) return <h1>something wrong have happened please try again</h1>;
+
   return (
     <main>
       <div className="pattern" />
@@ -24,9 +70,28 @@ function App() {
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without the Hassle
           </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="mt-15 ">
+          <h2 className="mt-[20px] text-center">All Movies</h2>
+          <div className="all-movies mt-10">
+            {loading ? (
+              <div className="flex items-center justify-center mt-10">
+                <Spinner />
+              </div>
+            ) : (
+              <ul>
+                {movies?.results.map((movie, index) => (
+                  <div key={index}>
+                    <MovieCard movie={movie} />
+                  </div>
+                ))}
+              </ul>
+            )}
+          </div>
+        </section>
       </div>
     </main>
   );
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function MovieCard({ movie }) {
+  return (
+    <div className="flex flex-col movie-card  gap-3 cursor-pointer">
+      <img
+        src={
+          movie?.poster_path
+            ? `https://media.themoviedb.org/t/p/w500/${movie.poster_path}`
+            : "../../public/no-movie.png"
+        }
+        alt="movie image"
+      />
+      <h3>{movie.title}</h3>
+      <div className="content gap-6 items-center">
+        <div className="rating">
+          <img src="../../public/star.svg" alt="rating-icon" />
+          <p>{movie.vote_average.toFixed(1)}</p>
+        </div>
+        <ul className="flex flex-row items-center gap-6 list-disc ">
+          <span>
+            <li>
+              {" "}
+              {movie?.original_language
+                ? movie?.original_language.toUpperCase()
+                : "N/A"}
+            </li>
+          </span>
+
+          <span>
+            <li>
+              {" "}
+              {movie?.release_date ? movie?.release_date.split("-")[0] : "N/A"}
+            </li>
+          </span>
+        </ul>
+      </div>
+    </div>
+  );
+}
